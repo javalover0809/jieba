04ce5ef1207a8378fa35e5c2ca40603abb03dd92
javalover0809 <280671026.com>
2021-01-26 23:46:20 +0800

    new

--- readExcel.py
+++ readExcel.py
@@ -1,16 +1,6 @@
 import xlrd
 import jieba.analyse
-
-ExcelFile = xlrd.open_workbook('/Users/Oraida/Desktop/yanglao.xls')
-
-print(ExcelFile.sheet_names())
-
-sheet = ExcelFile.sheet_by_name('整理324')
-
-rows_0 = sheet.row_values(0)
-print(rows_0)
-print(rows_0[1].strip())
-s = rows_0[1]
+import xlwt
 jieba.suggest_freq('养老服务', True)
 jieba.suggest_freq('养老机构', True)
 jieba.suggest_freq('机构养老', True)
@@ -21,21 +11,61 @@ jieba.suggest_freq('养老公寓', True)
 jieba.suggest_freq('养老保险', True)
 jieba.suggest_freq('社会养老', True)
 
-print(jieba.lcut(s))
 
-for x, w in jieba.analyse.extract_tags(s, withWeight=True):
+excel = xlwt.Workbook()
+
+
+ExcelFile = xlrd.open_workbook('/Users/Oraida/Desktop/yanglao.xls')
+sheet = ExcelFile.sheet_by_name('整理324')
+rows = sheet.row_values(11)
+
+date = rows[0]
+text = rows[1]
+
+new_excel = excel.add_sheet(date)
+
+new_excel.write(0, 0, "TF-IDF")
+new_excel.write(0, 1, "权重")
+
+new_excel.write(0, 2, "textRank")
+new_excel.write(0, 3, "权重")
+
+new_excel.write(0, 4, "字段")
+new_excel.write(0, 5, "次数")
+
+cnt = 1
+for x, w in jieba.analyse.extract_tags(text, topK=40, withWeight=True):
+    new_excel.write(cnt, 0, x)
+    new_excel.write(cnt, 1, w)
     print('%s  %s' % (x, w))
+    cnt = cnt + 1
 
-words = jieba.lcut(s)
+print("=================")
+
+cnt_textRank = 1
+for x, w in jieba.analyse.textrank(text, topK=40, withWeight=True):
+    new_excel.write(cnt_textRank, 2, x)
+    new_excel.write(cnt_textRank, 3, w)
+    print('%s  %s' % (x, w))
+    cnt_textRank = cnt_textRank + 1
+
+words = jieba.lcut(text)
 counts = {}
 for word in words:
-    if len(word) == 1:  #排除单个字符的分词结果
+    if len(word) == 1:
         continue
     else:
-        counts[word] = counts.get(word,0) + 1
+        counts[word] = counts.get(word, 0) + 1
 items = list(counts.items())
 items.sort(key=lambda x:x[1], reverse=True)
-for i in range(100):
+
+cnt_frequency = 1
+for i in range(50):
     word, count = items[i]
-    print(word , count)
-    # print ("{0:<10}{1:>5}".format(word, count))
+
+    new_excel.write(cnt_frequency, 4, word)
+    new_excel.write(cnt_frequency, 5, count)
+    print(word, count)
+    cnt_frequency = cnt_frequency + 1
+
+excel.save('/Users/Oraida/Desktop/first4.xls')
