69b3070a6e08e51eba8ad809e05b8022cb9a640a
javalover0809 <280671026.com>
2021-03-09 18:33:22 +0800

    new

--- 作业.py
+++ 作业.py
@@ -4,33 +4,22 @@ import random
 import matplotlib.pyplot as plt
 from PIL import Image
 
-path1 = 'D:\\tmp\cifar10_data\cifar-10-batches-py\data_batch_1'
-path2 = 'D:\\tmp\cifar10_data\cifar-10-batches-py\data_batch_2'
-path3 = 'D:\\tmp\cifar10_data\cifar-10-batches-py\data_batch_3'
-path4 = 'D:\\tmp\cifar10_data\cifar-10-batches-py\data_batch_4'
-path5 = 'D:\\tmp\cifar10_data\cifar-10-batches-py\data_batch_5'
-
-path6 = 'D:\\tmp\cifar10_data\cifar-10-batches-py\\test_batch'
+path1 = '/Users/Oraida/Desktop/模式识别/img.pkl'
 
 if __name__ == '__main__':
     with open(path1, 'rb') as fo:
         data = pickle.load(fo, encoding='bytes')
-
-        # print(data[b'batch_label'])
-        # print(data[b'labels'])
-        # print(data[b'data'])
-        # print(data[b'filenames'])
-
-        print(data[b'data'].shape)
-
-        images_batch = np.array(data[b'data'])
-        images = images_batch.reshape([-1, 3, 32, 32])
-        print(images.shape)
-        imgs = images[5, :, :, :].reshape([3, 32, 32])
-        img = np.stack((imgs[0, :, :], imgs[1, :, :], imgs[2, :, :]), 2)
-
-        print(img.shape)
-
-        plt.imshow(img)
+        data = np.array(data)
+        # print(data)
+        for i in range(0, 412):
+            # print(data[i])
+            for j in range(0, 549):
+                # print(data[i][j])
+                for k in range(0, 3):
+                    data[i][j][k] = (data[i][j][k] - 100)/2
+                    # print(data[i][j][k])
+        print(data)
+        print(data.shape)
+        plt.imshow(data)
         plt.axis('off')
         plt.show()
